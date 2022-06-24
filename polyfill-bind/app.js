0aecebd0b9e155c4d32267cc995e4afd7dd5107b
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-24 12:24:15 -0500

    [add] polyfill for bind method done

--- polyfill-bind/app.js
+++ polyfill-bind/app.js
@@ -3,18 +3,22 @@ let person = {
     lastname: "Benzema"
 };
 
-const printPerson = function() {
-    console.log(`${this.firstname} ${this.lastname}`);
+const printPerson = function(hometown, state) {
+    console.log(`${this.firstname} ${this.lastname}, ${hometown}, ${state}`);
 };
 
-const printMyPerson = printPerson.bind(person);
+const printMyPerson = printPerson.bind(person, "Barranquilla");
 printMyPerson();
 
+
 // Now write our own bind method
-Function.prototype.myBind() {
-    
+Function.prototype.customBind = function(...args) {
+    let targetFunction = this;
+    let params = args.slice(1);
+    return function () {
+        targetFunction.apply(args[0], params);
+    }
 }
 
-const printMyPerson2 = printPerson.myBind(person);
+const printMyPerson2 = printPerson.customBind(person, "Barranquilla");
 printMyPerson2();
-
